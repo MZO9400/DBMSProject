f2d2a46b42199c815f2a2f72563a792b977f4b7d
MZO9400 <hamza.hameed00@yahoo.com>
2020-06-23 14:41:11 +0500

    Implement basic GUI

--- WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/Form1.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace WinFormsApp1
+﻿using System.Data.SqlClient;
+
+namespace WinFormsApp1
 {
     partial class Form1
     {
@@ -24,16 +26,187 @@
         #region Windows Form Designer generated code
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.COMMIT = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize) (this.dataGridView1)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize) (this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(882, 591);
+            this.tabControl1.TabIndex = 5;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Controls.Add(this.COMMIT);
+            this.tabPage1.Controls.Add(this.comboBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(874, 565);
+            this.tabPage1.TabIndex = 1;
+            this.tabPage1.Text = "Tables";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Window;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 30);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(868, 532);
+            this.dataGridView1.TabIndex = 14;
+            // 
+            // COMMIT
+            // 
+            this.COMMIT.Location = new System.Drawing.Point(542, 6);
+            this.COMMIT.Name = "COMMIT";
+            this.COMMIT.Size = new System.Drawing.Size(324, 23);
+            this.COMMIT.TabIndex = 13;
+            this.COMMIT.Text = "COMMIT CHANGES";
+            this.COMMIT.UseVisualStyleBackColor = true;
+            this.COMMIT.Click += new System.EventHandler(this.COMMIT_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(8, 6);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(303, 21);
+            this.comboBox1.TabIndex = 12;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.richTextBox1);
+            this.tabPage2.Controls.Add(this.comboBox2);
+            this.tabPage2.Controls.Add(this.label1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(874, 565);
+            this.tabPage2.TabIndex = 2;
+            this.tabPage2.Text = "Recovered";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.richTextBox1.Location = new System.Drawing.Point(3, 33);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.ShortcutsEnabled = false;
+            this.richTextBox1.Size = new System.Drawing.Size(868, 529);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(109, 6);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 1;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(3, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "MR#";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.dataGridView2);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(874, 565);
+            this.tabPage3.TabIndex = 3;
+            this.tabPage3.Text = "Plasma";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.AllowUserToDeleteRows = false;
+            this.dataGridView2.BackgroundColor = System.Drawing.SystemColors.Info;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView2.Location = new System.Drawing.Point(3, 3);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.ReadOnly = true;
+            this.dataGridView2.Size = new System.Drawing.Size(868, 559);
+            this.dataGridView2.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(882, 591);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Hospital Management";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize) (this.dataGridView1)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize) (this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
         }
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+
+        private System.Windows.Forms.DataGridView dataGridView2;
+
+        private System.Windows.Forms.TabPage tabPage3;
+
+        private System.Windows.Forms.ComboBox comboBox2;
+
+        private System.Windows.Forms.Label label1;
+
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button COMMIT;
+        private System.Windows.Forms.DataGridView dataGridView1;
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+
+        private System.Data.SqlClient.SqlConnection connection;
 
         #endregion
     }
--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SqlClient;
 using System.Windows.Forms;
 
 namespace WinFormsApp1
@@ -15,6 +10,253 @@ namespace WinFormsApp1
         public Form1()
         {
             InitializeComponent();
+            _= this.initialize();
+        }
+
+        private bool initialize()
+        {
+            try
+            {
+                this.connection = new SqlConnection(new SqlConnectionStringBuilder
+                {
+                    DataSource = "HAMZAST440\\SQLSERVER",
+                    IntegratedSecurity = true,
+                    InitialCatalog = "DBMSLabExam"
+                }.ConnectionString);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show(@"Error connection to the local database", @"ERROR!");
+                return false;
+            }
+            this.fillComboBox();
+            this.fillPlasmaDonors();
+            return true;
+        }
+
+        private void fillPlasmaDonors()
+        {
+            connection.Open();
+            const string plasmaDonors =
+                @"SELECT * FROM REGISTERED_PATIENT REG INNER JOIN RECOVERED_PATIENTS REC ON REG.MR = REC.MR AND REC.DONATION_WILLINGNESS = 1";
+            SqlDataAdapter sda = new SqlDataAdapter(plasmaDonors, connection);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            this.dataGridView2.DataSource = dt;
+            connection.Close();
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e) {
+            try {
+                    connection.Open();
+                    string cmdstr = @"SELECT * FROM " + this.comboBox1.Text;
+                    SqlDataAdapter sda = new SqlDataAdapter(cmdstr, connection);
+                    DataTable dt = new DataTable();
+                    sda.Fill(dt);
+                    this.dataGridView1.DataSource = dt;
+                    connection.Close();
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void fillComboBox()
+        {
+            connection.Open();
+            const string fetchTableNames = @"SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME NOT IN ('SYSDIAGRAMS')";
+            DataTable dt = new DataTable();
+                SqlDataAdapter sda = new SqlDataAdapter(fetchTableNames, this.connection);
+                try {
+                    sda.Fill(dt);
+                    foreach (DataRow row in dt.Rows)
+                    {
+                        if (row != null) this.comboBox1.Items.Add(row["TABLE_NAME"]);
+                    }
+                }
+                catch (Exception ex) {
+                    MessageBox.Show(ex.Message);
+                }
+
+                const string fetchMRNo = "SELECT MR from [REGISTERED_PATIENT]";
+                sda = new SqlDataAdapter(fetchMRNo, this.connection);
+                try
+                {
+                    sda.Fill(dt);
+                    foreach (DataRow row in dt.Rows)
+                    {
+                        if (row != null && 
+                            (row.ItemArray[0] is DBNull ||
+                             string.IsNullOrWhiteSpace(row.ItemArray[0] as string))) 
+                            this.comboBox2.Items.Add(row["MR"]);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, @"MR Numbers not found");
+                    
+                }
+                
+            connection.Close();
+        }
+
+        private void COMMIT_Click(object sender, EventArgs e)
+        {
+            try {
+                connection.Open();
+                string cmdstr = @"SELECT * FROM [dbo].[" + this.comboBox1.Text + @"]";
+                SqlDataAdapter sda = new SqlDataAdapter(cmdstr, this.connection);
+                SqlCommandBuilder cmd = new SqlCommandBuilder(sda);
+                sda.InsertCommand = cmd.GetInsertCommand();
+                sda.DeleteCommand = cmd.GetDeleteCommand();
+                sda.UpdateCommand = cmd.GetUpdateCommand();
+                sda.Update((DataTable) this.dataGridView1.DataSource); 
+            }
+            catch (Exception err) {
+                MessageBox.Show(err.ToString(), @"ERROR");
+            }
+            connection.Close();
+        }
+
+        private void generateReport(string data)
+        {
+            string query =
+                @"SELECT * FROM REGISTERED_PATIENT PATIENT LEFT JOIN PATIENTS_INDOORS INDOOR ON PATIENT.MR = INDOOR.MR
+										 LEFT JOIN PATIENT_CHECKUP CHECKUP ON PATIENT.MR = CHECKUP.MR
+										 LEFT JOIN DEPARTMENT DEPT ON PATIENT.DEPARTMENT = DEPT.NAME
+										 LEFT JOIN SYMPTOMS SYMP ON PATIENT.MR = SYMP.MR
+										 LEFT JOIN RECOVERED_PATIENTS REC ON PATIENT.MR = REC.MR
+                                         WHERE PATIENT.MR=" + data + ";";
+            
+            try {
+                connection.Open();
+                SqlDataAdapter sda = new SqlDataAdapter(query, this.connection);
+                DataTable dt = new DataTable();
+                sda.Fill(dt);
+                DataRow row = dt.Rows[0];
+                string report = $"Patient MR Number: {row["MR"]}\n" +
+                                $"Name: {(row["FIRSTNAME"] as string)?.Trim()} {(row["LASTNAME"] as string)?.Trim()}\n" +
+                                $"Maiden Name: {row["FATHER_HUSBAND"]}\n" +
+                                $"Date of Birth: {row["DOB"]}\n" +
+                                $"Birth Place: {row["BIRTHPLACE"]}\n" +
+                                $"Blood Group: {row["BLOODGROUP"]}\n" +
+                                $"Address: {row["CURRENT_ADDRESS"]}\n" +
+                                $"Permanent Address: {row["PERMANENT_ADDRESS"]}\n" +
+                                $"District: {row["DISTRICT"]}\n" +
+                                $"Tehsil: {row["TEHSIL"]}\n" +
+                                $"State: {row["STATE"]}\n" +
+                                $"Caste: {row["CASTE"]}\n" +
+                                $"Highest Education: {row["EDUCATION"]}\n" +
+                                $"CNIC Number: {row["CNIC"]}\n" +
+                                $"Contact: {row["CONTACT"]}\n" +
+                                $"Mobile: {row["MOBILE"]}\n" +
+                                $"Email: {row["EMAIL"]}\n" +
+                                $"City: {row["CITY"]}\n";
+                var gender = (string) row["GENDER"];
+                if (gender != null && gender.IndexOf("M", StringComparison.Ordinal) != -1)
+                {
+                    gender = "Male";
+                }
+                else if (gender != null && gender.IndexOf("F", StringComparison.Ordinal) != -1)
+                {
+                    gender = "Female";
+                }
+                else
+                {
+                    gender = "Non binary";
+                }
+                report += $"Gender: {gender}\n" +
+                          $"Department: {row["DEPARTMENT"]}\n" +
+                          $"Doctor's diagnosis: {row["DIAGNOSIS"]}\n" +
+                          $"Doctor: {row["DOCTOR"]}\n" +
+                          $"Patient ID: {(row["PATIENTNUMBER"] as string)?.Trim()}\n";
+                if (!(row["PAYMENT_MODE"] is DBNull))
+                {
+                    report += "\n\n-------------\nINDOORS DATA\n--------\n\n" +
+                              $"Payment Mode: {row["PAYMENT_MODE"]}\n" +
+                              $"Room number: {row["Room"]}\n" +
+                              $"Admission Date: {row["ADMISSION_DATE"]}\n" +
+                              $"Initial Conditions: {row["INITIAL_CONDITIONS"]}\n" +
+                              $"INDOOR Doctor's diagnosis: {row["INDOOR_DIAGNOSIS"]}\n" +
+                              $"Treatment: {row["TREATMENT"]}\n" +
+                              $"Inspected by: {row["NUM_INSPECTING_DOCTORS"]} doctors\n" +
+                              $"Attendant: {row["ATTENDANT"]}\n";
+                }
+                if (!(row["CHECKUPDATE"] is DBNull))
+                {
+                    report += "\n\n-------------\nCHECKUP DATA\n--------\n\n" +
+                              $"DATE: {row["CHECKUPDATE"]}\n";
+                }
+
+                if (!(row["DRY_COUGH"] is DBNull))
+                {
+                    report += "\n\n-------------\nSYMPTOMS\n--------\n\n";
+                    if (((bool) row["DRY_COUGH"]))
+                    {
+                        report += "Dry Cough\n";
+                    }
+                    if (((bool) row["FEVER"]))
+                    {
+                        report += "Fever\n";
+                    }
+                    if (((bool) row["CHEST_PAIN"]))
+                    {
+                        report += "Chest pain\n";
+                    }
+                    if (((bool) row["SHORTENED_BREATH"]))
+                    {
+                        report += "Shortness of breath\n";
+                    }
+                    if (((bool) row["FATIGUE"]))
+                    {
+                        report += "Fatigue\n";
+                    }
+                    if (((bool) row["HEADACHE"]))
+                    {
+                        report += "Headache\n";
+                    }
+                    if (((bool) row["VOMITING"]))
+                    {
+                        report += "Vomiting\n";
+                    }
+                    if (((bool) row["LOSS_OF_SMELL"]))
+                    {
+                        report += "Loss of smell\n";
+                    }
+                    if (((bool) row["LOSS_OF_TASTE"]))
+                    {
+                        report += "Loss of taste\n";
+                    }
+                    if (((bool) row["DIARRHEA"]))
+                    {
+                        report += "Diarrhea\n";
+                    }
+                }
+
+                if (!(row["DONATION_WILLINGNESS"] is DBNull))
+                {
+                    report += "\n\n-------------\nDISCHARGE REPORT\n--------\n\n";
+                    if (((bool) row["DONATION_WILLINGNESS"]))
+                    {
+                        report += "Subject is willing to donate plasma\n";
+                    }
+                    else
+                    {
+                        report += "Subject is not willing to donate plasma\n";
+                    }
+
+                    report += $"Discharge date: {row["DATE_OF_DISCHARGE"]}\n";
+                }
+                this.richTextBox1.Text = report;
+
+            }
+            catch (Exception err) {
+                MessageBox.Show(err.ToString(), @"ERROR");
+            }
+            connection.Close();
+        }
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.generateReport(this.comboBox2.Text);
         }
     }
 }
